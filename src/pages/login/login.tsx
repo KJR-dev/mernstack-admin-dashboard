605ef1afa9357bff7d189184b6fe3c01d1e39210
Jitendra Sahoo <sahooj168@gmail.com>
2025-10-19 13:42:13 +0530

    add user data to store

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,6 +4,7 @@ import Logo from "../../components/icon/Logo";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import type { Credentials } from "../../types";
 import { login, self } from "../../http/api";
+import { useAuthStore } from "../../store";
 
 const loginUser = async (credentials: Credentials) => {
     // server call logic
@@ -17,6 +18,7 @@ const getSelf = async () => {
 }
 
 const LoginPage = () => {
+    const {setUser} = useAuthStore();
     const { data: selfData, refetch } = useQuery({
         queryKey: ["self"],
         queryFn: getSelf,
@@ -27,10 +29,9 @@ const LoginPage = () => {
         mutationFn: loginUser,
         onSuccess: async () => {
             // get self
-            refetch();
-            console.log("User data", selfData);
+            const selfDataPromise = await refetch();
             // store in the state
-            console.log("Login successful");
+            setUser(selfDataPromise.data);
         },
     })
 
