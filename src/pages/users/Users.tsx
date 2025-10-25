c8de8e59c1a7f117932c944b2a754fd6af33b47e
Jitendra Sahoo <sahooj168@gmail.com>
2025-10-25 12:44:31 +0530

    add loading spinner

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,7 @@
-import { Breadcrumb, Button, Drawer, Form, Space, Table, theme } from "antd";
-import { PlusOutlined, RightOutlined } from '@ant-design/icons';
+import { Breadcrumb, Button, Drawer, Flex, Form, Space, Spin, Table, theme, Typography } from "antd";
+import { LoadingOutlined, PlusOutlined, RightOutlined } from '@ant-design/icons';
 import { Link, Navigate } from "react-router-dom";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { keepPreviousData, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { createUser, getUsers } from "../../http/api";
 import type { CreateUserData, User } from "../../types";
 import { useAuthStore } from "../../store";
@@ -54,13 +54,13 @@ const Users = () => {
 
   })
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const { data: users, isLoading, isError, error } = useQuery({
+  const { data: users, isFetching, isError, error } = useQuery({
     queryKey: ["users", queryParams],
     queryFn: () => {
       const queryString = new URLSearchParams(queryParams as unknown as Record<string, string>).toString();
-      console.log('queryString', queryString);
       return getUsers(queryString).then((res) => res.data);
-    }
+    },
+    placeholderData: keepPreviousData,
   });
   const { user } = useAuthStore();
 
@@ -86,21 +86,23 @@ const Users = () => {
   return (
     <div>
       <Space direction="vertical" size={"large"} style={{ width: "100%" }}>
-        <Breadcrumb
-          separator={<RightOutlined />}
-          items={[
-            {
-              title: <Link to='/'>Dashboard</Link>,
-              // href: '/',
-            },
-            {
-              title: 'Users',
-              href: '#',
-            },
-          ]}
-        />
-        {isLoading && <div>Loading...</div>}
-        {isError && <div>{error.message}</div>}
+        <Flex justify='space-between'>
+          <Breadcrumb
+            separator={<RightOutlined />}
+            items={[
+              {
+                title: <Link to='/'>Dashboard</Link>,
+                // href: '/',
+              },
+              {
+                title: 'Users',
+                href: '#',
+              },
+            ]}
+          />
+          {isFetching && <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />}
+          {isError && <Typography.Text type="danger">{error.message}</Typography.Text >}
+        </Flex>
         <UsersFilter onFilterChange={(filterName: string, filterValue: string) => {
           console.log(filterName, filterValue);
         }}>
