225a89bc9e4f2bde259ffba9b6ad62a2711a427b
Jitendra Sahoo <sahooj168@gmail.com>
2025-11-09 20:40:30 +0530

    hide tenant if role admin

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import type { Tenant } from "../../../types";
 
 const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
+  const selectedRole = Form.useWatch('role',)
   const { data: tenants } = useQuery({
     queryKey: ["tenants"],
     queryFn: () => {
@@ -88,32 +89,35 @@ const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
                   placeholder="Select Role">
                   <Select.Option value="admin">Admin</Select.Option>
                   <Select.Option value="manager">Manager</Select.Option>
-                  <Select.Option value="customer">Customer</Select.Option>
                 </Select>
               </Form.Item>
             </Col>
-
-            <Col span={12}>
-              <Form.Item label="Tenant" name="tenantId" rules={[
-                {
-                  required: true,
-                  message: "Tenant is required"
-                }
-              ]}>
-                <Select style={{ width: '100%' }}
-                  id="selectBoxInUserForm"
-                  size="large"
-                  allowClear={true}
-                  onChange={() => { }}
-                  placeholder="Select Tenant">
-                  {tenants?.map((tenant: Tenant) =>
-                  (<Select.Option key={tenant.id} value={tenant.id}>
-                    {`${tenant.name}, ${tenant.address}`}
-                  </Select.Option>
-                  ))}
-                </Select>
-              </Form.Item>
-            </Col>
+            {
+              selectedRole === 'manager' && (
+                <Col span={12}>
+                  <Form.Item label="Tenant" name="tenantId" rules={[
+                    {
+                      required: true,
+                      message: "Tenant is required"
+                    }
+                  ]}>
+                    <Select style={{ width: '100%' }}
+                      id="selectBoxInUserForm"
+                      size="large"
+                      allowClear={true}
+                      onChange={() => { }}
+                      placeholder="Select Tenant">
+                      {tenants?.map((tenant: Tenant) =>
+                      (<Select.Option key={tenant.id} value={tenant.id}>
+                        {`${tenant.name}, ${tenant.address}`}
+                      </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              )
+            }
+            
           </Row>
         </Card>
 
